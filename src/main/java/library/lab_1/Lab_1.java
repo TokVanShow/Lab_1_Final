4810fa1f1d9bbde8748f76c4bb452948944884b8
TokVanShow <ivan.tokun@ya.ru>
2024-04-23 16:37:44 +0300

    Initial commit

---
+++ src/main/java/library/lab_1/Lab_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package library.lab_1;
+
+/**
+ *
+ * @author user
+ */
+public class Lab_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
